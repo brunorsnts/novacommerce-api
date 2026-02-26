c7ceafd2d69c45eb7e999537c47738df265e4289
brunorsnts <santos_2002@outlook.com.br>
2026-02-26 20:16:01 -0300

    feat: adiciona entidade Client com mapeamento JPA e validacoes

---
+++ src/main/java/com/novacommerce/ecommerce_api/entities/Client.java
@@ -0,0 +1,39 @@
+package com.novacommerce.ecommerce_api.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_clients")
+public class Client {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @CPF
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Setter(AccessLevel.NONE)
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+}
