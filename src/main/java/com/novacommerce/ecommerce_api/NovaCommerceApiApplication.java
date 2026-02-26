48d4dd45b39e6635c56e26002242fffc715d6210
brunorsnts <santos_2002@outlook.com.br>
2026-02-26 19:53:48 -0300

    chore: setup inicial do spring boot com dependencias

---
+++ src/main/java/com/novacommerce/ecommerce_api/NovaCommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.novacommerce.ecommerce_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaCommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaCommerceApiApplication.class, args);
+	}
+
+}
