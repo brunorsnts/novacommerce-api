a2170882747a8e224ebacf24a4d06c19ad7eacb7
brunorsnts <santos_2002@outlook.com.br>
2026-02-28 20:06:42 -0300

    feat: adiciona endpoint de busca de categoria por id

--- src/main/java/com/novacommerce/ecommerce_api/controllers/CategoryController.java
+++ src/main/java/com/novacommerce/ecommerce_api/controllers/CategoryController.java
@@ -4,6 +4,7 @@ import com.novacommerce.ecommerce_api.entities.Category;
 import com.novacommerce.ecommerce_api.repositories.CategoryRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,11 @@ public class CategoryController {
     public ResponseEntity<List<Category>> findAll() {
         List<Category> categories = repository.findAll();
         return ResponseEntity.ok(categories);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id) {
+        Category category = repository.findById(id).orElseThrow(() -> new RuntimeException("Categoria não encontrada"));
+        return ResponseEntity.ok(category);
     }
 }
